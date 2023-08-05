aa9a7b6abb72f8295e28d89c599e53584456b260
Bharat Sharma <Bharat.sharma@winshuttle.com>
2023-08-06 02:18:49 +0530

    Add project files.

---
+++ Employee/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using EmployeeView = Employee.Views.Employee;
+
+
+namespace Employee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainFrame.Navigate(new EmployeeView.Employee());
+        }
+    }
+}
